b221f0256af29bdcadc342fd83d5e5677c637b6a
tfermoso <tomas.fermoso@gmail.com>
2022-06-20 15:39:16 +0200

    heroku

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,11 @@ class Conexion {
     constructor() {
 
         this.con = mysql.createConnection({
-            host: "https://databases.000webhost.com/",
-            user: "id18920129_admin_loginchat",
-            database:"id18920129_loginchat",
-            password: "}q5W?$pof&#%O>C#"
+            host: "ec2-52-22-136-117.compute-1.amazonaws.com",
+            user: "bqdadughsjsqod",
+            database:"d1ign1h9o3fuv5",
+            password: "7b6e288a9259c080aeb5d2890149d127efb5cd8b9b576c699edfdce2336a397c",
+            port: 5432
         });
 
         this.con.connect(function (err) {
